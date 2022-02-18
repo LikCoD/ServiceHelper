9fd3e170e4f38ebe425fbe3a8c598e657d906ee6
LikDan <likdan.official@gmail.com>
2022-02-18 03:22:02 -0800

    Improve load screen

--- src/main/kotlin/ldcapps/servicehelper/controllers/LoadingView.kt
+++ src/main/kotlin/ldcapps/servicehelper/controllers/LoadingView.kt
@@ -3,6 +3,7 @@ package ldcapps.servicehelper.controllers
 import javafx.application.Platform
 import javafx.geometry.Pos
 import javafx.scene.Parent
+import javafx.scene.control.Label
 import javafx.scene.text.Font
 import ldcapps.servicehelper.db.DataClasses
 import ldcapps.servicehelper.styles.MainStyle
@@ -12,14 +13,28 @@ import kotlin.concurrent.thread
 
 class LoadingView : View() {
 
+    private lateinit var stateLabel: Label
+
     override val root: Parent = vbox(spacing = 10) {
         addClass(MainStyle.mainWindow)
 
         alignment = Pos.CENTER
         paddingAll = 10
 
-        label("Загрузка..."){
-            font = Font.font(24.0)
+        anchorpane {
+            label("Загрузка...") {
+                anchorpaneConstraints {
+                    rightAnchor = 5
+                    leftAnchor = 5
+                }
+                font = Font.font(24.0)
+            }
+            stateLabel = label("Подключение"){
+                anchorpaneConstraints {
+                    rightAnchor = 5
+                    bottomAnchor = 0
+                }
+            }
         }
         progressbar(-1.0){
             maxWidth = Double.MAX_VALUE
@@ -28,14 +43,26 @@ class LoadingView : View() {
 
     init {
         thread {
+            setState("Компании")
             DataClasses.companies = sqList()
+
+            setState("Физ. лица")
             DataClasses.individuals = sqList()
+
+            setState("Владельцы")
             DataClasses.owners = sqList()
+
+            setState("Авто")
             DataClasses.cars = sqList()
 
+            setState("Отчеты")
             DataClasses.reports = sqList()
 
             Platform.runLater { replaceWith<MainView>(sizeToScene = true, centerOnScreen = true) }
         }
+    }
+
+    private fun setState(state: String){
+        Platform.runLater { stateLabel.text = state }
     }
 }
